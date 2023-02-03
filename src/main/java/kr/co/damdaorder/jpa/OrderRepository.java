2c3bf07a86d84429591de2fef971aff6d3df0910
홍혁준 <jjj8337721@naver.com>
2023-02-03 17:41:51 +0900

    feat : Swagger 기능

--- src/main/java/kr/co/damdaorder/jpa/OrderRepository.java
+++ src/main/java/kr/co/damdaorder/jpa/OrderRepository.java
@@ -3,12 +3,14 @@ package kr.co.damdaorder.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     //find
     Optional<OrderEntity> findByOrderCode (String orderCode);
+    List<OrderEntity> findByIdentity(String identity);
 
     //exists
     boolean existsByOrderCode(String orderCode);
