eb90bef09d508f30323d850db2a4875fa54b73a4
홍혁준 <jjj8337721@naver.com>
2023-02-06 20:20:43 +0900

    feat : feign client 적용

--- src/main/java/kr/co/damdaorder/DamdaOrderApplication.java
+++ src/main/java/kr/co/damdaorder/DamdaOrderApplication.java
@@ -2,8 +2,12 @@ package kr.co.damdaorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class DamdaOrderApplication {
 
     public static void main(String[] args) {
